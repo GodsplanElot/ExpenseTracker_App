96aa0be941a66677fc55f2e3a9297fe2bc968c91
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-09 16:55:40 +0100

    Style and add filter function

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import ExpenseList from "./expense-tracker/components/ExpenseList"
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter"
 
 
 function App() {
   const [expenses, setExpenses] = useState(
     [
-    {id: 1, description: 'aaa', amount: 100, category: 'utilities'},
+    {id: 1, description: 'aaa', amount: 10, category: 'utilities'},
     {id: 2, description: 'bbb', amount: 10, category: 'utilities'},
     {id: 3, description: 'ccc', amount: 10, category: 'utilities'},
     {id: 4, description: 'ddd', amount: 10, category: 'utilities'},
@@ -13,6 +14,10 @@ function App() {
   
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={category => console.log(category)}></ExpenseFilter>
+      </div>
+      
       <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
     </div>
       
