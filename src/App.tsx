a68e6f5b45bd43ec923fe2b0ca4306dc66aa0bb9
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-09 14:02:02 +0100

    Add DELETE functionality to the expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import { useState } from "react"
 import ExpenseList from "./expense-tracker/components/ExpenseList"
 
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState(
+    [
     {id: 1, description: 'aaa', amount: 100, category: 'utilities'},
     {id: 2, description: 'bbb', amount: 10, category: 'utilities'},
     {id: 3, description: 'ccc', amount: 10, category: 'utilities'},
     {id: 4, description: 'ddd', amount: 10, category: 'utilities'},
-  ]
+  ])
   
-
   return (
     <div>
-      <ExpenseList expenses={expenses} onDelete={(id) => console.log('Delete', id)} />
+      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
     </div>
       
     
