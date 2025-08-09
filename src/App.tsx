e9b24c36dbaf108f7d24d12b32d0138a2decada0
Godsplan Elot <elotgodsplan2@gmail.com>
2025-08-09 19:10:37 +0100

    Create Form to add expenses

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import ExpenseList from "./expense-tracker/components/ExpenseList"
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter"
 
+const categories = ["Groceries", "Utilities", "Entertainment"];
+
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState('')
---
+++ src/expense-tracker/components/ExpenseForm.tsx
@@ -0,0 +1,30 @@
+
+
+const ExpenseForm = () => {
+  return (
+    <div>
+      <form>
+
+        <div className="mb-3">
+        <label htmlFor="" className="form-label">Description</label>
+        <input id="description" type="text" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+        <label htmlFor="amount" className="form-label">Amount</label>
+        <input id="amount" type="number" className="form-control" />
+        </div>
+
+        <div className="mb-3">
+        <label htmlFor="category" className="form-label">Category</label>
+        <select id="category"  className="form-control" >
+            
+        </select>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default ExpenseForm
